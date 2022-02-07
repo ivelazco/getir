767832139b7c1df3e4e0204587b3f98fe0f2a38e
ignacio-velazco <ignacio.velazco@compass.com>
2022-02-07 17:51:09 +0100

    chore: add comments

--- src/search/search.handler.js
+++ src/search/search.handler.js
@@ -2,6 +2,18 @@ import { getMongoDBDatabase, logger } from '../utils';
 
 const log = logger('search-endpoint');
 
+/**
+ * getFilteredRecords function
+ * Get from the collection `records` the values that accomplish the match setted.
+ * Getting the records that have the ` createdAt` field between the given `startDate and `endDate` and also
+ * with a `totalCount` between the given `minCount` and `maxCount`.
+ * @date 2022-02-07
+ * @param {string} startDate The minimum `createdAt` date to search.
+ * @param {string} endDate The maximum `createdAt` date to search.
+ * @param {number} minCount The minimum `totalCount` (the sum of counts array) number to search.
+ * @param {number} maxCount The maximum `totalCount` (the sum of counts array) number to search.
+ * @returns {Array<{key: string, createdAt: string, totalCount: number}>} the results that match with the filters.
+ */
 async function getFilteredRecords({ startDate, endDate, minCount, maxCount }) {
     const db = await getMongoDBDatabase();
     return db
@@ -31,6 +43,14 @@ async function getFilteredRecords({ startDate, endDate, minCount, maxCount }) {
         .toArray();
 }
 
+/**
+ * searchHandler function
+ * Handle the search endpoint, formatting the different responses that the endpoint could have
+ * @date 2022-02-07
+ * @param {any} {body} Express Request Object
+ * @param {any} res Express Response Object
+ * @returns {any}
+ */
 async function searchHandler({ body }, res) {
     // eslint-disable-next-line no-console
     log(`Request params received:`, body);
